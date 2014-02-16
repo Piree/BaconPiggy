b55f6abe337b8093e7a3918b64138d4624efcd95
Pierre <pierre@Pierres-Mac.local>
2014-02-16 10:50:15 -0800

    Initial Commit

---
+++ Doodle Jump/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Doodle Jump
+//
+//  Created by Pierre on 2014-02-16.
+//  Copyright (c) 2014 Pierre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Doodle Jump/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Doodle Jump
+//
+//  Created by Pierre on 2014-02-16.
+//  Copyright (c) 2014 Pierre. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
